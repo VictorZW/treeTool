fb3e7cf0ade78bbae5cc97352b9868b7e0e83bc1
VictorZW <zwzhangwei1@gmail.com>
2020-12-19 09:29:39 +0800

    some

--- js/index.js
+++ js/index.js
@@ -11,12 +11,17 @@
                 return true
             }
             return false // 如果为空,返回false
+        },
+        // 生成随机数
+        getRandomNum: function (Min,Max) {
+            var Range = Max - Min
+            var Rand = Math.random()
+            return (Min + Math.round(Rand * Range))
         }
     }
     var treeUtils = {
         initTree: function (el, options) {
             console.log(options)
-            console.log(utils.checkOptions(options))
             // 往el dom中插入 创建项目 元素 只能插入一次
             var addProjectOne = utils.creatDom('<div class="addProject-one">创建项目</div>')
             el.appendChild(addProjectOne)
@@ -24,25 +29,39 @@
             // 如果options不为空，去循环渲染dom
             if (utils.checkOptions(options)) {
                 for (var i in options) {
-                    console.log(i)
+                    // 将一级树渲染到dom
                     var treeOne = treeUtils.initTreeOne(i)
                     treeOne && addProjectOne.insertAdjacentHTML('beforebegin', treeOne)
-                    console.log(options[i])
+                    var treeOneId = treeOne.match(/\d+/)[0]
+                    var treeTwoAddDom = document.getElementById(treeOneId)
+                    console.log(treeTwoAddDom)
+                    // 如果options[i]是数组，说明有子项
+                    if (options[i] instanceof Array) {
+                        for (var j in options[i]) {
+                            // console.log(options[i][j])
+                            for (var k in options[i][j]) {
+                                // console.log(k)
+                                var treeTwo = treeUtils.initTreeTwo(treeOneId, k)
+                                console.log(treeTwo)
+                                treeTwo && treeTwoAddDom.insertAdjacentHTML('beforebegin', treeTwo)
+                            }
+                        }
+                    }
                 }
             }
 
             // 点击事件：点击创建项目按钮 创建一级树
             addProjectOne.addEventListener('click', function () {
                 var treeOne = treeUtils.initTreeOne()
-                console.log(treeOne)
-                console.log(addProjectOne)
+                // console.log(treeOne)
+                // console.log(addProjectOne)
                 treeOne && addProjectOne.insertAdjacentHTML('beforebegin', treeOne)
             })
 
             // 点击事件：事件委托
             document.addEventListener('click', function (event) {
                 var target = event.target
-                console.log(target, target.className, target.dataset.id)
+                // console.log(target, target.className, target.dataset.id)
                 // 点击创建子项按钮
                 if (target.className === 'addProject-two') {
                     // 点击的是创建子项
@@ -55,7 +74,8 @@
         initTreeOne: function (name) {
             name = name || prompt('请输入要创建的名字','')
             if (name !== null) {
-                var timestamp = new Date().getTime()
+                // 生成100000-999999的随机数
+                var timestamp = utils.getRandomNum(10000,999999)
                 var treeOne = '<div class="tree-one" data-id="'+ timestamp + '">' +
                         '<div class="left">' +
                             '<div class="arrow" data-id="'+ timestamp + '"></div>' +
@@ -73,10 +93,11 @@
                 return false
             }
         },
-        initTreeTwo: function (id) {
-            console.log(id)
+        // 添加二级树型图
+        initTreeTwo: function (id, subName) {
+            // console.log(id)
             var subId = 'sub' + String(id)
-            var subName = prompt('请输入要创建的子项名字','')
+            subName = subName || prompt('请输入要创建的子项名字','')
             if (subName !== null) {
                 var treeTwo = '<div class="tree-two" data-id="'+ subId + '">' +
                     '<div class="left">' +
